39a262da91dcf8dd7aab5336312a8d16b52dfbd8
Augustus175 <columbia7311@163.com>
2019-01-22 23:32:21 +0800

    2019-01-22 23:31:42

--- src/main/java/com/zzb/javacore/chap14/bounce/BallComponent.java
+++ src/main/java/com/zzb/javacore/chap14/bounce/BallComponent.java
@@ -1,4 +1,31 @@
 package com.zzb.javacore.chap14.bounce;
 
-public class BallComponent {
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallComponent extends JPanel {
+    private static final int DEFAULT_WIDTH = 450;
+    private static final int DEFAULT_HIGHT = 350;
+
+    private List<Ball> balls = new ArrayList<>();
+
+    public void add(Ball ball) {
+        balls.add(ball);
+    }
+
+    public void painComponent(Graphics graphics) {
+        super.printComponent(graphics);
+        Graphics2D g2 = (Graphics2D) graphics;
+        for (Ball b :
+                balls) {
+            g2.fill(b.getShap());
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HIGHT);
+    }
 }
--- src/main/java/com/zzb/javacore/chap14/bounce/Bounce.java
+++ src/main/java/com/zzb/javacore/chap14/bounce/Bounce.java
@@ -1,10 +1,42 @@
 package com.zzb.javacore.chap14.bounce;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Bounce {
 
 }
 
 class BounceFrame extends JFrame {
+    private BallComponent comp;
+    public static final int STEPS = 1000;
+    public static final int DELAY = 3;
+
+    public BounceFrame() {
+        setTitle("Bounce");
+        comp = new BallComponent();
+        add(comp, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+    }
+
+    public void addButton(Container c, String title, ActionListener listener) {
+        JButton button = new JButton(title);
+        c.add(button);
+        button.addActionListener(listener);
+    }
+
+    public void addBall() {
+        Ball ball = new Ball();
+        comp.add(ball);
+        for (int i = 0; i < STEPS; i++) {
+            ball.move(comp.getBounds());
+            comp.paint(comp.getGraphics());
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
