125f8d5b0da2e97f73026fed7c823f6df87383ca
Augustus175 <columbia7311@163.com>
2019-01-23 23:38:01 +0800

    |2019-01-23 23:37:23

--- src/main/java/com/zzb/javacore/chap14/bounce/Bounce.java
+++ src/main/java/com/zzb/javacore/chap14/bounce/Bounce.java
@@ -5,7 +5,13 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Bounce {
-
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new BounceFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
 
 class BounceFrame extends JFrame {
