965ec67b363378d3e0e95fcbc266cf9d6aa8dae7
gavin <columbia7311@163.com>
2019-02-02 09:46:32 +0800

    2019-02-02 09:45:30

--- src/main/java/com/zzb/javacore/chap14/banckdemo/FutureTest.java
+++ src/main/java/com/zzb/javacore/chap14/banckdemo/FutureTest.java
@@ -12,7 +12,24 @@ import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class FutureTest {
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter base directory (e.g. /usr/local/) : ");
+        String directory = in.nextLine();
+        System.out.println("Enter keyword (e.g volatile): ");
+        String keyword = in.nextLine();
+        MatchCounter counter = new MatchCounter(new File(directory), keyword);
+        FutureTask<Integer> task = new FutureTask<>(counter);
+        Thread t = new Thread(task);
+        t.start();
+        try {
+            System.out.println(task.get() + " matching files.");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 class MatchCounter implements Callable<Integer> {
